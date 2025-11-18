ad579259a6b08fa193ed976548c46cac9661b145
alcan <alcan@example.com>
2025-11-18 10:53:44 +0100

    - feat(bank-transfer-repo): ajouter la récupération de l’historique par compte trié par date

--- src/main/java/com/alcaniz/paymybuddy/repository/BankTransferRepository.java
+++ src/main/java/com/alcaniz/paymybuddy/repository/BankTransferRepository.java
@@ -2,11 +2,13 @@ package com.alcaniz.paymybuddy.repository;
 
 import com.alcaniz.paymybuddy.model.BankTransfer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface BankTransferRepository extends JpaRepository<BankTransfer, Integer> {
-    // Optionnel V1: historique par compte
+    //  historique par compte
     List<BankTransfer> findAllByAccount_IdOrderByCreatedAtDesc(Integer accountId);
 
 }
