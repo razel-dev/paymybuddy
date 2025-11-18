75635b34466d4a41f517d86b2574b9a86a43a4c9
alcan <alcan@example.com>
2025-11-18 11:00:01 +0100

    - eat(repository): ajouter TransactionRepository avec requête d’historique triée par date

--- src/main/java/com/alcaniz/paymybuddy/repository/TransactionRepository.java
+++ src/main/java/com/alcaniz/paymybuddy/repository/TransactionRepository.java
@@ -2,10 +2,11 @@ package com.alcaniz.paymybuddy.repository;
 
 import com.alcaniz.paymybuddy.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-// Java
+@Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
     //  historique simple
     List<Transaction> findAllBySenderAccount_IdOrReceiverAccount_IdOrderByCreatedAtDesc(
