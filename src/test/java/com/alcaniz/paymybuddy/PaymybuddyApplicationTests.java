92685e85caa448c44e042006824aa34c894265ea
alcan <alcan@example.com>
2025-12-10 08:31:59 +0100

    test

--- src/test/java/com/alcaniz/paymybuddy/PaymybuddyApplicationTests.java
+++ src/test/java/com/alcaniz/paymybuddy/PaymybuddyApplicationTests.java
@@ -1,9 +1,14 @@
 package com.alcaniz.paymybuddy;
 
+import com.alcaniz.paymybuddy.infra.MySqlTestcontainersConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@Import(MySqlTestcontainersConfig.class)
 class PaymybuddyApplicationTests {
 
     @Test
